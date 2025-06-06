17b05b12b57afa85d147d31e9acfc1ba88a7ceb9
Андрей Цуканов <a.tsukanov@hibirit.com>
2025-06-06 16:39:56 +0300

    Remove tool

--- CarCompaniesApplication.API/Tool/ExceptionMessageTool.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CarCompaniesApplication.API.Tool;
-
-public static class ExceptionMessageTool
-{
-    public const string AddCompanyMessage = "Произошла ошибка при добавлении компании";
-}
